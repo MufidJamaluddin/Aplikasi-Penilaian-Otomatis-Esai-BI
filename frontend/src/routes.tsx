0d058388b71358cdf08ac1c9f2ad9b3c4b4bce61
luckyramdani <lucky.ramdani.tif16@polban.ac.id>
2019-03-25 14:37:57 +0700

    add page tambah soal

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -7,6 +7,7 @@ const Siswa = React.lazy(() => import('./views/Siswa/Siswa'));
 const Kelas = React.lazy(() => import('./views/Kelas/Kelas'));
 const Ujian = React.lazy(() => import('./views/Ujian/Ujian'));
 const TambahUjian = React.lazy(() => import('./views/Ujian/TambahUjian'));
+const BuatSoal = React.lazy(() => import('./views/Ujian/BuatSoal'));
 const Soal = React.lazy(() => import('./views/Soal/Soal'));
 const Matapelajaran = React.lazy(() => import('./views/Matapelajaran/Matapelajaran'));
 const Users = React.lazy(() => import('./views/Users/Users'));
@@ -20,9 +21,10 @@ const routes = [
   { path: '/siswa', name: 'Siswa', component: Siswa },
   { path: '/kelas', name: 'Kelas', component: Kelas },
   { path: '/ujian', name: 'Ujian', component: Ujian },
-  { path: '/tambahujian', name: 'TambahUjian', component: TambahUjian },
+  { path: '/buatsoal', name: 'Buat Soal', component: BuatSoal },
+  { path: '/tambahujian', name: 'Tambah Ujian', component: TambahUjian },
   { path: '/soal', name: 'Soal', component: Soal },
-  { path: '/matapelajaran', name: 'Matapelajaran', component: Matapelajaran },
+  { path: '/matapelajaran', name: 'Mata Pelajaran', component: Matapelajaran },
   { path: '/users', exact: true,  name: 'Users', component: Users },
   { path: '/users/:id', exact: true, name: 'User Details', component: User },
 ];
