8aa92bd3aeeb1bacdae48691e5059d71a9ad0dca
luckyramdani007 <luckyramdani007@gmail.com>
2019-05-03 15:08:40 +0700

    fixing minor

--- frontend/src/admin_views/Kelas/Kelas.tsx
+++ frontend/src/admin_views/Kelas/Kelas.tsx
@@ -58,203 +58,110 @@ class Kelas extends Component<KelasModel, KelasStateModeel>
         <Row>
           <Col xs="12" lg="12">
             <Card>
-              <CardHeader>
-
-				 <Col md="4">
-                      <InputGroup>
-                        <Input type="text" id="search" name="search" placeholder="Cari Kelas..." />
-						<Button type="button" color="primary"><i className="fa fa-search"></i></Button>
-						 </InputGroup>
-                    </Col>
-
-              </CardHeader>
-			  
               <CardBody>
-			  <Form action="" method="post" className="form-horizontal">
-				<FormGroup row>
-				<Col sm="4">
-					<Input type="text" placeholder="Nama Kelas" required/>
-				</Col>
-				<Col sm="2">
-					<Button bsSize="sm" color="success" className="px-4"><i className="fa fa-plus"></i><span>Tambah Kelas</span></Button>
-				</Col>
-				
-				</FormGroup>
-			</Form>	
+								<Form action="" method="post" className="form-horizontal">
+									<FormGroup row>
+										<Col sm="4">
+											<Input type="text" placeholder="Nama Kelas" required/>
+										</Col>
+										<Col sm="2">
+											<Button bsSize="sm" color="success" className="px-4"><i className="fa fa-plus"></i><span>Tambah Kelas</span></Button>
+										</Col>
+									</FormGroup>
+								</Form>	
                 <Table responsive reflow size="sm">
                   <thead>
                   <tr>
                     <th>Nama Kelas</th>
-					<th></th>
-					<th></th>
-					<th></th>
-					
-					<th>Aksi</th>
-                  </tr>
+										<th></th>
+										<th></th>
+										<th></th>
+										<th>Aksi</th>
+									</tr>
                   </thead>
                   <tbody>
                   <tr>
                 	<td>X-IPA1</td>
-					<td></td>
-					<td></td>
-                	<td></td>
-					
-                	
-					<td>
-						
-				<Button className="btn-stack-overflow btn-brand icon btn-sm" onClick={this.toggleUpdateKelas}><i className="fa fa-pencil"></i></Button>
-				<Modal isOpen={this.state.warning} toggle={this.toggleUpdateKelas}className={'modal-warning  ' + this.props.className}>
-							  
-				  <ModalHeader toggle={this.toggleUpdateKelas}>Update Kelas</ModalHeader>
-                  <ModalBody>
-                    <Form action="" method="post" className="form-horizontal">
-					  <FormGroup row>
-						<Col sm="12">
-						  <p><Input  type="text" placeholder="Nama Kelas" required /></p>
-						</Col>
-					  </FormGroup>
-					</Form>
-					
-                  </ModalBody>		  
-				  <ModalFooter>
-						<Button color="danger" onClick={this.toggleUpdateKelas}>Cancel</Button>
-						<Button color="success" onClick={this.toggleUpdateKelas}>Update</Button>{' '}
-				  </ModalFooter>
-				</Modal>
-						
-						
-				<Button className="btn-youtube btn-brand icon btn-sm" onClick={this.toggleDeleteKelas}><i className="fa fa-trash"></i></Button>
-				<Modal isOpen={this.state.danger} toggle={this.toggleDeleteKelas}
-                       className={'modal-danger ' + this.props.className}>
-                  <ModalHeader toggle={this.toggleDeleteKelas}>Delete Kelas</ModalHeader>
-                  <ModalBody>
-					<p> Apakah anda yakin ingin menghapus <b>XI-IPA1</b> dari data Kelas ?</p>
-                  </ModalBody>
-                  <ModalFooter>
-				    <Button color="danger" onClick={this.toggleDeleteKelas}>Tidak</Button>
-                    <Button color="success" onClick={this.toggleDeleteKelas}>Ya</Button>{' '}
-                  </ModalFooter>
-                </Modal>
-					</td>
+									<td></td>
+									<td></td>
+                	<td></td>   	
+									<td>
+											
+									<Button className="btn-stack-overflow btn-brand icon btn-sm" onClick={this.toggleUpdateKelas}><i className="fa fa-pencil"></i></Button>
+									<Modal isOpen={this.state.warning} toggle={this.toggleUpdateKelas}className={'modal-warning  ' + this.props.className}>
+													
+										<ModalHeader toggle={this.toggleUpdateKelas}>Update Kelas</ModalHeader>
+											<ModalBody>
+												<Form action="" method="post" className="form-horizontal">
+													<FormGroup row>
+														<Col sm="12">
+															<p><Input  type="text" placeholder="Nama Kelas" required /></p>
+														</Col>
+													</FormGroup>
+												</Form>					
+                  	</ModalBody>		  
+										<ModalFooter>
+											<Button color="danger" onClick={this.toggleUpdateKelas}>Cancel</Button>
+											<Button color="success" onClick={this.toggleUpdateKelas}>Update</Button>{' '}
+										</ModalFooter>
+									</Modal>
+			
+									<Button className="btn-youtube btn-brand icon btn-sm" onClick={this.toggleDeleteKelas}><i className="fa fa-trash"></i></Button>
+									<Modal isOpen={this.state.danger} toggle={this.toggleDeleteKelas} className={'modal-danger ' + this.props.className}>
+											<ModalHeader toggle={this.toggleDeleteKelas}>Delete Kelas</ModalHeader>
+											<ModalBody><p> Apakah anda yakin ingin menghapus <b>XI-IPA1</b> dari data Kelas ?</p></ModalBody>
+                  		<ModalFooter>
+												<Button color="danger" onClick={this.toggleDeleteKelas}>Tidak</Button>
+												<Button color="success" onClick={this.toggleDeleteKelas}>Ya</Button>{' '}
+											</ModalFooter>
+                	</Modal>
+									</td>
                   </tr>
-                  <tr>
+									
+									<tr>
                     <td>X-IPA1</td>
-					<td></td>
-					<td></td>
-                    <td></td>
+										<td></td>
+										<td></td>
+										<td></td>
                 	<td>
-						<Button className="btn-stack-overflow btn-brand icon btn-sm"><i className="fa fa-pencil"></i></Button>
-						<Button className="btn-youtube btn-brand icon btn-sm"><i className="fa fa-trash"></i></Button>
-					</td>
-                  </tr><tr>
+										<Button className="btn-stack-overflow btn-brand icon btn-sm"><i className="fa fa-pencil"></i></Button>
+										<Button className="btn-youtube btn-brand icon btn-sm"><i className="fa fa-trash"></i></Button>
+									</td>
+									</tr>
+									
+									<tr>
                     <td>X-IPA1</td>
-					<td></td>
-					<td></td>
+										<td></td>
+										<td></td>
+										<td></td>
+            	    	<td>
+											<Button className="btn-stack-overflow btn-brand icon btn-sm"><i className="fa fa-pencil"></i></Button>
+											<Button className="btn-youtube btn-brand icon btn-sm"><i className="fa fa-trash"></i></Button>
+										</td>
+									</tr>
+									
+									<tr>
+                    <td>X-IPA1</td>
+										<td></td>
+										<td></td>
                     <td></td>
-                	<td>
-						<Button className="btn-stack-overflow btn-brand icon btn-sm"><i className="fa fa-pencil"></i></Button>
-						<Button className="btn-youtube btn-brand icon btn-sm"><i className="fa fa-trash"></i></Button>
-					</td>
-                  </tr><tr>
+	                	<td>
+											<Button className="btn-stack-overflow btn-brand icon btn-sm"><i className="fa fa-pencil"></i></Button>
+											<Button className="btn-youtube btn-brand icon btn-sm"><i className="fa fa-trash"></i></Button>
+										</td>
+									</tr>
+									
+									<tr>
                     <td>X-IPA1</td>
-					<td></td>
-					<td></td>
+										<td></td>
+										<td></td>
                     <td></td>
-                	<td>
-						<Button className="btn-stack-overflow btn-brand icon btn-sm"><i className="fa fa-pencil"></i></Button>
-						<Button className="btn-youtube btn-brand icon btn-sm"><i className="fa fa-trash"></i></Button>
-					</td>
-                  </tr><tr>
-                    <td>X-IPA1</td>
-					<td></td>
-					<td></td>
-                    <td></td>
-                	<td>
-						<Button className="btn-stack-overflow btn-brand icon btn-sm"><i className="fa fa-pencil"></i></Button>
-						<Button className="btn-youtube btn-brand icon btn-sm"><i className="fa fa-trash"></i></Button>
-					</td>
-                  </tr><tr>
-                    <td>X-IPA1</td>
-					<td></td>
-					<td></td>
-                    <td></td>
-                	<td>
-						<Button className="btn-stack-overflow btn-brand icon btn-sm"><i className="fa fa-pencil"></i></Button>
-						<Button className="btn-youtube btn-brand icon btn-sm"><i className="fa fa-trash"></i></Button>
-					</td>
-                  </tr><tr>
-                    <td>X-IPA1</td>
-					<td></td>
-					<td></td>
-                    <td></td>
-                	<td>
-						<Button className="btn-stack-overflow btn-brand icon btn-sm"><i className="fa fa-pencil"></i></Button>
-						<Button className="btn-youtube btn-brand icon btn-sm"><i className="fa fa-trash"></i></Button>
-					</td>
-                  </tr><tr>
-                    <td>X-IPA1</td>
-					<td></td>
-					<td></td>
-                    <td></td>
-                	<td>
-						<Button className="btn-stack-overflow btn-brand icon btn-sm"><i className="fa fa-pencil"></i></Button>
-						<Button className="btn-youtube btn-brand icon btn-sm"><i className="fa fa-trash"></i></Button>
-					</td>
-                  </tr><tr>
-                    <td>X-IPA1</td>
-					<td></td>
-					<td></td>
-                    <td></td>
-                	<td>
-						<Button className="btn-stack-overflow btn-brand icon btn-sm"><i className="fa fa-pencil"></i></Button>
-						<Button className="btn-youtube btn-brand icon btn-sm"><i className="fa fa-trash"></i></Button>
-					</td>
-                  </tr><tr>
-                    <td>X-IPA1</td>
-					<td></td>
-					<td></td>
-                    <td></td>
-                	<td>
-						<Button className="btn-stack-overflow btn-brand icon btn-sm"><i className="fa fa-pencil"></i></Button>
-						<Button className="btn-youtube btn-brand icon btn-sm"><i className="fa fa-trash"></i></Button>
-					</td>
-                  </tr><tr>
-                    <td>X-IPA1</td>
-					<td></td>
-					<td></td>
-                    <td></td>
-                	<td>
-						<Button className="btn-stack-overflow btn-brand icon btn-sm"><i className="fa fa-pencil"></i></Button>
-						<Button className="btn-youtube btn-brand icon btn-sm"><i className="fa fa-trash"></i></Button>
-					</td>
-                  </tr><tr>
-                    <td>X-IPA1</td>
-					<td></td>
-					<td></td>
-                	<td></td>
-                    <td>
-						<Button className="btn-stack-overflow btn-brand icon btn-sm"><i className="fa fa-pencil"></i></Button>
-						<Button className="btn-youtube btn-brand icon btn-sm"><i className="fa fa-trash"></i></Button>
-					</td>
-                  </tr><tr>
-                    <td>X-IPA1</td>
-					<td></td>
-                	<td></td>
-					<td></td>
-                    <td>
-						<Button className="btn-stack-overflow btn-brand icon btn-sm"><i className="fa fa-pencil"></i></Button>
-						<Button className="btn-youtube btn-brand icon btn-sm"><i className="fa fa-trash"></i></Button>
-					</td>
-                  </tr><tr>
-                    <td>X-IPA1</td>
-					<td></td>
-                	<td></td>
-					<td></td>
-                    <td>
-						<Button className="btn-stack-overflow btn-brand icon btn-sm"><i className="fa fa-pencil"></i></Button>
-						<Button className="btn-youtube btn-brand icon btn-sm"><i className="fa fa-trash"></i></Button>
-					</td>
-                  </tr>
+										<td>
+											<Button className="btn-stack-overflow btn-brand icon btn-sm"><i className="fa fa-pencil"></i></Button>
+											<Button className="btn-youtube btn-brand icon btn-sm"><i className="fa fa-trash"></i></Button>
+										</td>
+									</tr>
+									
                   </tbody>
                 </Table>
               </CardBody>
--- frontend/src/admin_views/Matapelajaran/Matapelajaran.tsx
+++ frontend/src/admin_views/Matapelajaran/Matapelajaran.tsx
@@ -60,17 +60,7 @@ class Matapelajaran extends Component<MatapelajaranModel, MatapelajaranStateMode
         <Row>
           <Col xs="12" lg="12">
             <Card>
-              <CardHeader>
-
-				 <Col md="4">
-                      <InputGroup>
-                        <Input type="text" id="search" name="search" placeholder="Cari Mata Pelajaran..." />
-						<Button type="button" color="primary"><i className="fa fa-search"></i></Button>
-						 </InputGroup>
-                    </Col>
-
-              </CardHeader>
-			  
+         
               <CardBody>
 			  <Form action="" method="post" className="form-horizontal">
 				<FormGroup row>
@@ -85,142 +75,85 @@ class Matapelajaran extends Component<MatapelajaranModel, MatapelajaranStateMode
 				</FormGroup>
 			</Form>	
         <Table responsive size="sm">
-                  <thead>
-                  <tr>
-                    <th>Nama Mata Pelajaran</th>
-					<th></th>
-					<th></th>
-					<th></th>
-					
-					<th>Aksi</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                  <tr>
-                	<td>Geografi</td>
-					<td></td>
-					<td></td>
-                	<td></td>
-					
-                	
-					<td>
-						
-				<Button className="btn-stack-overflow btn-brand icon btn-sm" onClick={this.toggleUpdateMatapelajaran}><i className="fa fa-pencil"></i></Button>
-				<Modal isOpen={this.state.warning} toggle={this.toggleUpdateMatapelajaran}className={'modal-warning  ' + this.props.className}>
-							  
-				  <ModalHeader toggle={this.toggleUpdateMatapelajaran}>Update Mata Pelajaran</ModalHeader>
-          <ModalBody>
-            <Form action="" method="post" className="form-horizontal">
-              <FormGroup row>
-              <Col sm="12">
-                <p><Input  type="text" placeholder="Nama Mata Pelajaran" required /></p>
-              </Col>
-              </FormGroup>
-            </Form>
-          </ModalBody>		  
-				  <ModalFooter>
-						<Button color="danger" onClick={this.toggleUpdateMatapelajaran}>Cancel</Button>
-						<Button color="success" onClick={this.toggleUpdateMatapelajaran}>Update</Button>{' '}
-				  </ModalFooter>
-				</Modal>
-						
-						
+          <thead>
+            <tr>
+              <th>Nama Mata Pelajaran</th>
+              <th></th>
+              <th></th>
+              <th></th>       
+              <th>Aksi</th>
+            </tr>
+          </thead>
+          
+          <tbody>
+            <tr>
+              <td>Geografi</td>
+              <td></td>
+              <td></td>
+              <td></td>        	
+					    <td>
+              <Button className="btn-stack-overflow btn-brand icon btn-sm" onClick={this.toggleUpdateMatapelajaran}><i className="fa fa-pencil"></i></Button>
+              <Modal isOpen={this.state.warning} toggle={this.toggleUpdateMatapelajaran}className={'modal-warning  ' + this.props.className}>
+                      
+                <ModalHeader toggle={this.toggleUpdateMatapelajaran}>Update Mata Pelajaran</ModalHeader>
+                <ModalBody>
+                  <Form action="" method="post" className="form-horizontal">
+                    <FormGroup row>
+                    <Col sm="12">
+                      <p><Input  type="text" placeholder="Nama Mata Pelajaran" required /></p>
+                    </Col>
+                    </FormGroup>
+                  </Form>
+                </ModalBody>		  
+                <ModalFooter>
+                  <Button color="danger" onClick={this.toggleUpdateMatapelajaran}>Cancel</Button>
+                  <Button color="success" onClick={this.toggleUpdateMatapelajaran}>Update</Button>{' '}
+                </ModalFooter>
+              </Modal>
 				<Button className="btn-youtube btn-brand icon btn-sm" onClick={this.toggleDeleteMatapelajaran}><i className="fa fa-trash"></i></Button>
-				<Modal isOpen={this.state.danger} toggle={this.toggleDeleteMatapelajaran}
-                       className={'modal-danger ' + this.props.className}>
-                  <ModalHeader toggle={this.toggleDeleteMatapelajaran}>Delete Mata Pelajaran</ModalHeader>
-                  <ModalBody>
-					<p> Apakah anda yakin ingin menghapus <b>XI-IPA1</b> dari data Mata Pelajaran ?</p>
-                  </ModalBody>
-                  <ModalFooter>
+				<Modal isOpen={this.state.danger} toggle={this.toggleDeleteMatapelajaran} className={'modal-danger ' + this.props.className}>
+          <ModalHeader toggle={this.toggleDeleteMatapelajaran}>Delete Mata Pelajaran</ModalHeader>
+          <ModalBody><p> Apakah anda yakin ingin menghapus <b>XI-IPA1</b> dari data Mata Pelajaran ?</p></ModalBody>
+          <ModalFooter>
 				    <Button color="danger" onClick={this.toggleDeleteMatapelajaran}>Tidak</Button>
-                    <Button color="success" onClick={this.toggleDeleteMatapelajaran}>Ya</Button>{' '}
-                  </ModalFooter>
-                </Modal>
-					</td>
-                  </tr>
-                  <tr>
-                    <td>Geografi</td>
-					<td></td>
-					<td></td>
-                    <td></td>
+            <Button color="success" onClick={this.toggleDeleteMatapelajaran}>Ya</Button>{' '}
+          </ModalFooter>
+        </Modal>
+				        </td>
+              </tr>
+               
+                <tr>
+                  <td>Geografi</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
                 	<td>
-						<Button className="btn-stack-overflow btn-brand icon btn-sm"><i className="fa fa-pencil"></i></Button>
-						<Button className="btn-youtube btn-brand icon btn-sm"><i className="fa fa-trash"></i></Button>
-					</td>
-                  </tr><tr>
-                    <td>Geografi</td>
-					<td></td>
-					<td></td>
-                	<td></td>
-                    <td>
-						<Button className="btn-stack-overflow btn-brand icon btn-sm"><i className="fa fa-pencil"></i></Button>
-						<Button className="btn-youtube btn-brand icon btn-sm"><i className="fa fa-trash"></i></Button>
-					</td>
-                  </tr><tr>
-                    <td>Geografi</td>
-					<td></td>
-                	<td></td>
-					<td></td>
-                    <td>
-						<Button className="btn-stack-overflow btn-brand icon btn-sm"><i className="fa fa-pencil"></i></Button>
-						<Button className="btn-youtube btn-brand icon btn-sm"><i className="fa fa-trash"></i></Button>
-					</td>
-                  </tr><tr>
-                    <td>Geografi</td>
-					<td></td>
-                	<td></td>
-					<td></td>
-                    <td>
-						<Button className="btn-stack-overflow btn-brand icon btn-sm"><i className="fa fa-pencil"></i></Button>
-						<Button className="btn-youtube btn-brand icon btn-sm"><i className="fa fa-trash"></i></Button>
-					</td>
-                  </tr><tr>
-                    <td>Geografi</td>
-					<td></td>
-					<td></td>
-                	<td></td>
-                    <td>
-						<Button className="btn-stack-overflow btn-brand icon btn-sm"><i className="fa fa-pencil"></i></Button>
-						<Button className="btn-youtube btn-brand icon btn-sm"><i className="fa fa-trash"></i></Button>
-					</td>
-                  </tr><tr>
-                    <td>Geografi</td>
-					<td></td>
-					<td></td>
-                	<td></td>
-                    <td>
-						<Button className="btn-stack-overflow btn-brand icon btn-sm"><i className="fa fa-pencil"></i></Button>
-						<Button className="btn-youtube btn-brand icon btn-sm"><i className="fa fa-trash"></i></Button>
-					</td>
-                  </tr><tr>
-                    <td>Geografi</td>
-					<td></td>
-					<td></td>
-                	<td></td>
-                    <td>
-						<Button className="btn-stack-overflow btn-brand icon btn-sm"><i className="fa fa-pencil"></i></Button>
-						<Button className="btn-youtube btn-brand icon btn-sm"><i className="fa fa-trash"></i></Button>
-					</td>
-                  </tr><tr>
-                    <td>Geografi</td>
-					<td></td>
-					<td></td>
-                	<td></td>
-                    <td>
-						<Button className="btn-stack-overflow btn-brand icon btn-sm"><i className="fa fa-pencil"></i></Button>
-						<Button className="btn-youtube btn-brand icon btn-sm"><i className="fa fa-trash"></i></Button>
-					</td>
-                  </tr><tr>
-                    <td>Geografi</td>
-					<td></td>
-					<td></td>
-                	<td></td>
-                    <td>
-						<Button className="btn-stack-overflow btn-brand icon btn-sm"><i className="fa fa-pencil"></i></Button>
-						<Button className="btn-youtube btn-brand icon btn-sm"><i className="fa fa-trash"></i></Button>
-					</td>
-                  </tr>
+                    <Button className="btn-stack-overflow btn-brand icon btn-sm"><i className="fa fa-pencil"></i></Button>
+                    <Button className="btn-youtube btn-brand icon btn-sm"><i className="fa fa-trash"></i></Button>
+                  </td>
+                </tr>
+                <tr>
+                <td>Geografi</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>
+                  <Button className="btn-stack-overflow btn-brand icon btn-sm"><i className="fa fa-pencil"></i></Button>
+                  <Button className="btn-youtube btn-brand icon btn-sm"><i className="fa fa-trash"></i></Button>
+                </td>
+              </tr>
+
+              <tr>
+              <td>Geografi</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td>
+                <Button className="btn-stack-overflow btn-brand icon btn-sm"><i className="fa fa-pencil"></i></Button>
+                <Button className="btn-youtube btn-brand icon btn-sm"><i className="fa fa-trash"></i></Button>
+              </td>
+            </tr>
+          
                   </tbody>
                 </Table>
               </CardBody>
