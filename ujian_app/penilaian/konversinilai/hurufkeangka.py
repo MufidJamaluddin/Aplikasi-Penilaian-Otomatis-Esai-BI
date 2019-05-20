36c7c9c415f18b0a69cc561510f0e5e2c3c74b71
nurindah yuniarti <31493745+NurindahYuniarti2A@users.noreply.github.com>
2019-05-20 08:14:26 -0700

    hurufkeangka

--- ujian_app/penilaian/konversinilai/hurufkeangka.py
+++ ujian_app/penilaian/konversinilai/hurufkeangka.py
@@ -1,5 +1,5 @@
 '''
-ambil nilai maks dan nilai min dari setiap soal
+ambil nilai maks dan nilai min dari setiap soal 
 buat tabel konversi dengan rumus max-min/4
 ambil seluruh skor huruf pada soal tertentu
 hitung jumlah kemunculan tiap hurufnya
@@ -10,3 +10,27 @@ urutkan descending
 beri masing2 skor huruf dengan nilai angka
 looping
 '''
+
+class SkorHurufKeAngka(object):
+    def __init__(self, skorMax, skorMin, jumlahKelas=4):
+        self.skorMax = skorMax
+        self.skorMin = skorMin
+        self.jumlahKelas = jumlahKelas
+
+    def get_interval_predikal(self):
+        '''
+        Mendapatkan interval predikat pada 4 kelas
+        '''
+        interval = (self.skorMax - self.skorMin) / self.jumlahKelas
+        return interval
+    
+    def jumlah_skor_huruf(self, skorHuruf):
+        '''
+        Mendapatkan jumlah kemunculan skor A/B/C/D pada jawaban siswa
+        '''
+    
+    def konversi(self):
+        '''
+        
+        '''
+        rentang = 
--- ujian_app/penilaian/konversinilai/konversi.py
+++ ujian_app/penilaian/konversinilai/konversi.py
@@ -1,5 +1,6 @@
 from ujian_app.models import Soal
 from .angkakehuruf import SkorAngkaKeHuruf
+from .hurufkeangka import SkorHurufKeAngka
 
 class Konversi(object):
     """
@@ -16,12 +17,13 @@ class Konversi(object):
 
         soal = Soal.query.get(self.idsoal)
         self.skor_angka_ke_huruf = SkorAngkaKeHuruf(soal.skorMax, soal.skorMin)
+        self.skor_huruf_ke_angka = SkorHurufKeAngka(soal.skorMax, soal.skorMin)
 
     def ke_angka(self, huruf:str):
         '''
         Melakukan konversi nilai dari huruf ke angka
         '''
-        pass
+        return self.skor_huruf_ke_angka.konversi(huruf)
 
     def ke_huruf(self, angka:int):
         '''
