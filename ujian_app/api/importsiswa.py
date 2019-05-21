829343bffde477b0d15398983c528bef2953b185
nurindah yuniarti <31493745+NurindahYuniarti2A@users.noreply.github.com>
2019-05-20 19:43:35 -0700

    nambah file

--- ujian_app/api/importsiswa.py
+++ ujian_app/api/importsiswa.py
@@ -8,36 +8,3 @@ from copy import deepcopy, copy
 from flask import Flask, request, jsonify
 from flask.ext import excel
 
-class ImportGuruAPI(MethodView):
-    def get_wb_data_guru(self):
-        workbook = load_workbook("template/Data_Siswa.xlsx")
-        sheet = workbook.active
-
-        workbook.remove(sheet)
-        return workbook
-
-    def get(self):
-        file_name = 'Data_Guru.xlsx'
-        workbook = self.get_wb_data_guru()
-        with NamedTemporaryFile() as tmp:
-            workbook.save(tmp)
-            tmp.seek(0)
-            return Response(
-                tmp.read(),
-                mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-                headers={'Content-Disposition':'attachment; filename="%s"' % (file_name)}
-            )
-    
-    def upload_file(self):
-        if request.method == 'POST'
-        return jsonify({'result': request.get_array(field_name='file')})
-        return '''
-    <!doctype html>
-    <title>Upload an excel file</title>
-    <h1>Excel file upload (csv, tsv, csvz, tsvz only)</h1>
-    <form action="" method=post enctype=multipart/form-data><p>
-    <input type=file name=file><input type=submit value=Upload>
-    </form>
-    '''
-        
-
