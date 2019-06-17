1665991fd24e3dd60d0cb18d37f96975a5b102b9
luckyramdani007 <luckyramdani007@gmail.com>
2019-06-17 08:02:33 +0700

    fixing dataset

--- penilaian_knn.py
+++ penilaian_knn.py
@@ -1,3 +1,4 @@
+from ujian_app.penilaian import PenilaianOtomatis
 from ujian_app import make_app
 from ujian_app.tasks import penilaian_otomatis
 from ujian_app.models import Ujian
@@ -10,4 +11,8 @@ print('Mulai penilaian otomatis dg KNN ...')
 
 app = make_app()
 
-penilaian_otomatis.apply_async(args=[ID_UJIAN])
+with app.app_context():
+    potomatis = PenilaianOtomatis(ID_UJIAN)
+    potomatis.nilai_otomatis()
+
+#penilaian_otomatis.apply_async(args=[ID_UJIAN])
