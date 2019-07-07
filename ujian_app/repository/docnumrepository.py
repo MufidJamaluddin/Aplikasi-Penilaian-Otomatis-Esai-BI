abfd1d55a45266e769497e89339740d70ef94df5
mufidjamaluddin <mufid.jamaluddin@gmail.com>
2019-07-08 00:03:13 +0700

    Fix LruCache

--- ujian_app/repository/docnumrepository.py
+++ ujian_app/repository/docnumrepository.py
@@ -3,12 +3,17 @@ from sqlalchemy.sql.expression import and_
 from ujian_app.models import ( 
     FiturReferensiPenilaian, Jawaban, db
 )
+from functools import lru_cache
 
 class DocNumRepository(object):
     '''
     Kemunculan Dokumen
     '''
+    def clear_lrucache(self):
+        self.get_doc_num_neg_class.cache_clear()
+        self.get_doc_num_pos_class.cache_clear()
 
+    @lru_cache()
     def get_doc_num_pos_class(self, idsoal, term, skorHuruf):
         """
         Mendapatkan Jumlah Dokumen Kelas Positif (Kelas Skor Huruf/Alfabet)
@@ -29,7 +34,8 @@ class DocNumRepository(object):
         if jml_doc_positif is None:
             return 0
         return jml_doc_positif
-    
+
+    @lru_cache()
     def get_doc_num_neg_class(self, idsoal, term, skorHuruf):
         """
         Mendapatkan Jumlah Dokumen Kelas Negatif (Kelas Skor Huruf/Alfabet)
