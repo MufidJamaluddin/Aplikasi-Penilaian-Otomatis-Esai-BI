b8da69d5922f327a541be82387c62db0e7a2b522
mufidjamaluddin <mufid.jamaluddin@gmail.com>
2019-07-08 15:41:51 +0700

    Add Resume Capaian Siswa dg Boxplot & Add Mobx

--- frontend/src/views/laporan/LaporanUjian.tsx
+++ frontend/src/views/laporan/LaporanUjian.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Card, CardBody, CardHeader,Form, Col, Collapse,  Row, Table, Button, Input } from 'reactstrap';
-import { Line } from 'react-chartjs-2';
+import ChartComponent, { Line } from 'react-chartjs-2';
 import DaftarNilaiUjian from '../../models/item_model';
 
 import DataPengampu from '../../models/item_model';
@@ -10,6 +10,9 @@ import { initDaftarnilaiujian, downloadDaftarnilaiujian } from '../../models/Daf
 import FormGroup from 'reactstrap/lib/FormGroup';
 import CardFooter from 'reactstrap/lib/CardFooter';
 
+require('chart.js');
+require('chartjs-chart-box-and-violin-plot');
+
 //-------------------------------//
 interface GrafikGarisProps { nama: string; nilai_ujian: any; list_ujian: any, key: string;  }
 
@@ -18,7 +21,9 @@ class GrafikGaris extends React.PureComponent<GrafikGarisProps>
   render() 
   {
     const list_nilai = Object.values(this.props.nilai_ujian);
-    const list_ujian = Object.keys(this.props.nilai_ujian).map(key => { return this.props.list_ujian[key] });
+    const list_ujian = Object.keys(this.props.nilai_ujian).map(key => { 
+      return this.props.list_ujian[key] 
+    });
 
     const card_data = {
       labels: list_ujian,
@@ -38,6 +43,46 @@ class GrafikGaris extends React.PureComponent<GrafikGarisProps>
         width={30}
         options={{ maintainAspectRatio: false }} 
         />
+    );
+  }
+}
+
+interface GrafikBoxplotProps 
+{
+  list_nilaiujian: any;
+}
+
+class GrafikBoxplot extends React.PureComponent<GrafikBoxplotProps>
+{
+  chartInstance?: any;
+  
+  render() 
+  {
+    const labels = Object.keys(this.props.list_nilaiujian)
+    
+    var i = -1;
+    const data_t = labels.map(label => {
+      i += 1;
+      return this.props.list_nilaiujian[label] || [];
+    });
+
+    const card_data = {
+      labels: labels,
+      datasets: [{
+        label: 'Resume Capaian Siswa',
+        borderWidth: 1,
+        borderColor: 'rgba(19,141,117,.55)',
+        backgroundColor: 'rgba(115,198,182,.55)',
+        data: data_t
+      }]
+    };
+
+    return (
+      <ChartComponent
+        type='boxplot'
+        data={card_data}
+        ref={ref => this.chartInstance = ref && ref.chartInstance}
+      />
     );
   }
 }
@@ -117,7 +162,8 @@ class LaporanUjian extends Component<LaporanUjianModel, LaporanUjianStateModel>
       initDaftarnilaiujian(nextState.idmapel_selected, nextState.idkelas_selected).then(data => {
         this.setState({ 
           list_nilai: data.list_nilai,
-          list_ujian: data.list_ujian
+          list_ujian: data.list_ujian,
+          list_nilaiujian: data.list_nilaiujian
         });
       });  
     }
@@ -185,6 +231,18 @@ class LaporanUjian extends Component<LaporanUjianModel, LaporanUjianStateModel>
     );
   }
 
+  render_boxplot()
+  {
+    if (this.state.list_nilaiujian == undefined)
+    return;
+
+    return (
+      <GrafikBoxplot
+        list_nilaiujian={this.state.list_nilaiujian}
+      />
+    );
+  }
+
   render_grafik()
   {
     return (
@@ -193,7 +251,7 @@ class LaporanUjian extends Component<LaporanUjianModel, LaporanUjianStateModel>
           this.state.list_nilai.map(dt_nilai => {
             return (
               <Card key={dt_nilai.nis}>
-                <CardHeader>{dt_nilai.nama}</CardHeader>
+                <CardHeader>{dt_nilai.nis} | {dt_nilai.nama}</CardHeader>
                 <CardBody>
                   <GrafikGaris 
                     key={dt_nilai.nis}
@@ -211,6 +269,19 @@ class LaporanUjian extends Component<LaporanUjianModel, LaporanUjianStateModel>
         }
       </div>
     )
+  }
+
+  render_body()
+  {
+    switch(this.state.tampilan)
+    {
+      case "1":
+        return this.render_table();
+      case "2":
+        return this.render_grafik();
+      case "0":
+        return this.render_boxplot();
+    }
   }
 
   render()
@@ -246,8 +317,9 @@ class LaporanUjian extends Component<LaporanUjianModel, LaporanUjianStateModel>
                   </Col>
                   <Col className="col-sm-3">
                       <Input type="select" onChange={e=>{this.setState({tampilan: e.target.value})}} defaultValue="0">
-                        <option>Tabel Nilai Ujian</option>
-                        <option>Grafik Nilai Ujian</option>
+                      <option value="0">Resume Capaian Siswa</option>
+                        <option value="1">Tabel Nilai Ujian</option>
+                        <option value="2">Perkembangan Nilai Ujian Siswa</option>
                       </Input>
                   </Col>
                   
@@ -263,8 +335,7 @@ class LaporanUjian extends Component<LaporanUjianModel, LaporanUjianStateModel>
 
               <CardBody>
                 {
-                  this.state.tampilan == '0' ?
-                    this.render_table() : this.render_grafik()
+                  this.render_body()
                 }
               </CardBody>
             </Card>
